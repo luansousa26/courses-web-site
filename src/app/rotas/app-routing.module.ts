fba785967f2d4ef6a9be6fabc959e3232b6f4c51
Luan Sousa <luan26@uninove.edu.br>
2018-07-18 21:23:42 -0300

    rotas implementadas

--- src/app/rotas/app-routing.module.ts
+++ src/app/rotas/app-routing.module.ts
@@ -1,8 +1,12 @@
+import { SobreComponent } from './../telas/sobre/sobre.component';
+import { CursosComponent } from './../telas/cursos/cursos.component';
 import { HomeComponent } from './../telas/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'cursos', component: CursosComponent},
+  { path: 'sobre', component: SobreComponent}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
