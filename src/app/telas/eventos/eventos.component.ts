0f7227c33b78778efe95f4ea181aa3f7e66c6055
Luan Sousa <luan26@uninove.edu.br>
2018-07-23 20:10:01 -0300

    Alteração

--- src/app/telas/eventos/eventos.component.ts
+++ src/app/telas/eventos/eventos.component.ts
@@ -37,22 +37,22 @@ export class EventosComponent implements OnInit {
     this.arrayLinhaUm = [
       {
         imagem: 'assets/eventos/formatura.jpg', titulo: 'Encontro dos Formandos',
-        informacoes: 'O aluno(a) estará apto a operar computadores como seus programas e reeventos.', linha: 1
+        informacoes: 'O evento de formatura da turma B de informática  ocorreu no dia 10/07/2018.', linha: 1
       },
       { imagem: 'assets/eventos/ballet.jpg', titulo: 'Baile dos Cisnes',
-       informacoes: 'O aluno(a) aprenderá sobre história geral.', linha: 1 },
+       informacoes: 'Apresentação de nossos alunos de Ballet clássico.', linha: 1 },
       { imagem: 'assets/eventos/criancas.jpg', titulo: 'Dias das Crianças',
-       informacoes: 'O aluno(a) aprenderá a dominar o inglês.', linha: 1 },
+       informacoes: 'Traga seus filhos para conheçerem nossa escola e eles se divertirão com nossa programação de dia das crianças.', linha: 1 },
       {
         imagem: 'assets/eventos/idosos.jpg', titulo: 'Festa dos Idosos',
-        informacoes: 'O aluno(a) estará apto(a) a conversar em frânces fluentemente.', linha: 1
+        informacoes: 'Quem disse que os maiores de 50 não de divertem? venham conosco ao baile dos idosos.', linha: 1
       }
     ];
     this.arrayLinhaDois = [
-      { imagem: 'assets/eventos/hiphop.jpg', titulo: 'Show de HipHop', informacoes: 'O aluno(a) aprenderá a dominar o Russo.', linha: 2 },
+      { imagem: 'assets/eventos/hiphop.jpg', titulo: 'Show de HipHop', informacoes: 'Show de Hip-Hop dos nossos alunos', linha: 2 },
       {
         imagem: 'assets/eventos/palestra.jpg', titulo: 'Palestra',
-        informacoes: 'O aluno(a) será treinado para poder atuar em um hospital.', linha: 2
+        informacoes: 'Palestra sobre Tecnologias para internet.', linha: 2
       },
       { imagem: 'assets/eventos/pinturas.jpg', titulo: 'Pinturas', informacoes: 'O aluno(a) aprenderá pintura.', linha: 2 },
       { imagem: 'assets/eventos/sangue.jpg', titulo: 'Doação de sangue', informacoes: 'O aluno(a) aprenderá bordado.', linha: 2 }
