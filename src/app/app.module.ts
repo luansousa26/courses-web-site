6e9e6b05aa8cc818689950aba9ce8fceca5ff46d
Luan Sousa <luan26@uninove.edu.br>
2018-08-31 22:41:56 -0300

    translate added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
 import { NavbarComponent } from './layout/navbar/navbar.component';
@@ -13,6 +13,12 @@ import { FooterComponent } from './layout/footer/footer.component';
 import { CursoDialogComponent } from './telas/curso-dialog/curso-dialog.component';
 import { EventosComponent } from './telas/eventos/eventos.component';
 import { BackTopComponent } from './layout/back-top/back-top.component';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,6 +36,14 @@ import { BackTopComponent } from './layout/back-top/back-top.component';
     BrowserAnimationsModule,
     MaterialModule,
     AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyDamuwvDFPTTlR9Cv1RUAQxR-z3dntAeu8'
     })
--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,26 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '../../../../node_modules/@angular/router';
+  import { Component, OnInit } from '@angular/core';
+  import { Router } from '../../../../node_modules/@angular/router';
+  import { TranslateService } from '@ngx-translate/core';
 
-@Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
-})
-export class NavbarComponent implements OnInit {
+  @Component({
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss']
+  })
+  export class NavbarComponent implements OnInit {
 
-  controla: boolean;
-  constructor(public router: Router) { }
+    controla: boolean;
+    constructor(public router: Router,
+                public translate: TranslateService) {
+                  this.translate.addLangs(['pt-br', 'fr']);
+                  this.translate.setDefaultLang('pt-br');
+                }
 
-  ngOnInit() {
-  }
-  openNav() {
-    if (this.controla === true) {
-      document.getElementById('mySidenav').style.width = '0';
-      this.controla = false;
-    } else {
-      document.getElementById('mySidenav').style.width = '250px';
-      this.controla = true;
+    ngOnInit() {
     }
-  }
+    openNav() {
+      if (this.controla === true) {
+        document.getElementById('mySidenav').style.width = '0';
+        this.controla = false;
+      } else {
+        document.getElementById('mySidenav').style.width = '250px';
+        this.controla = true;
+      }
+    }
 
-}
+  }
