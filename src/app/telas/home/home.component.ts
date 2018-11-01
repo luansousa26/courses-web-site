07adfc8b64c5d806af8de0780159725073a391a4
Luan Sousa <luan26@uninove.edu.br>
2018-11-01 11:45:38 -0300

    Alteração no método de alterar imagens

--- src/app/telas/home/home.component.ts
+++ src/app/telas/home/home.component.ts
@@ -1,49 +1,60 @@
-import { Component, OnInit, Inject, HostListener } from '@angular/core';
-import { DOCUMENT } from '@angular/platform-browser';
-import { OnDestroy } from '@angular/core';
+import { Component, OnInit, Inject, HostListener } from "@angular/core";
+import { DOCUMENT } from "@angular/platform-browser";
+import { OnDestroy } from "@angular/core";
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit, OnDestroy {
-
   imagensDisponiveis: any[] = [];
   controlador = -1;
   lat = -23.2828262;
   lng = -46.7455076;
   navIsFixed: boolean;
   controle: any;
-  constructor(@Inject(DOCUMENT) private document: Document) { }
+  constructor(@Inject(DOCUMENT) private document: Document) {}
 
   ngOnInit() {
     this.imagensDisponiveis = [
-      'assets/background/imagem1.jpg',
-      'assets/background/imagem2.jpg',
-      'assets/background/imagem3.jpg',
-      'assets/background/imagem5.jpg'
+      "assets/background/imagem1.jpg",
+      "assets/background/imagem2.jpg",
+      "assets/background/imagem3.jpg",
+      "assets/background/imagem5.jpg"
     ];
-    this.controle = setInterval(() => {this.startTimer(); }, 7000);
+    this.controle = setInterval(() => {
+      this.startTimer();
+    }, 7000);
   }
   ngOnDestroy() {
-  clearInterval(this.controle);
+    clearInterval(this.controle);
   }
- private displayNextImage(): void  {
-    (<HTMLImageElement>document.getElementById('img')).style.opacity = '0.1';
-    (<HTMLImageElement>document.getElementById('img')).style.transition = 'opacity 1s linear';
+  private displayNextImage(): void {
+    const styleCss = <HTMLImageElement>document.getElementById("img");
+    styleCss.style.opacity = "0.1";
+    styleCss.style.transition = "opacity 1s linear";
+    // Timeout para alterar a imagem
     setTimeout(() => {
-      this.controlador = (this.controlador === this.imagensDisponiveis.length - 1) ? 0 : this.controlador + 1;
-      (<HTMLImageElement>document.getElementById('img')).src = this.imagensDisponiveis[this.controlador];
-      (<HTMLImageElement>document.getElementById('img')).style.opacity = '1';
+      this.controlador =
+        this.controlador === this.imagensDisponiveis.length - 1
+          ? 0
+          : this.controlador + 1;
+      styleCss.src = this.imagensDisponiveis[this.controlador];
+      styleCss.style.opacity = "1";
     }, 600);
   }
 
   private displayPreviousImage(): void {
-    this.controlador = (this.controlador <= 0) ? this.imagensDisponiveis.length - 1 : this.controlador - 1;
-    (<HTMLImageElement>document.getElementById('img')).src = this.imagensDisponiveis[this.controlador];
+    this.controlador =
+      this.controlador <= 0
+        ? this.imagensDisponiveis.length - 1
+        : this.controlador - 1;
+    (<HTMLImageElement>(
+      document.getElementById("img")
+    )).src = this.imagensDisponiveis[this.controlador];
   }
 
   private startTimer(): void {
-      this.displayNextImage();
+    this.displayNextImage();
   }
 }
